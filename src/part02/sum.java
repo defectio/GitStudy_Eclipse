abb278548b4fb1b02612d53859cf448fe032dd4e
defectio <defectio916@gmail.com>
2024-07-05 10:40:56 +0900

    sum 브랜치에 소스 추가

---
+++ src/part02/sum.java
@@ -0,0 +1,13 @@
+package part02;
+
+public class sum {
+
+	public static void main(String[] args) {
+		
+		int sum = 0;
+		for(int i = 1; i < 11; i++) {
+			sum += i;
+		}
+		System.out.println("sum : "+sum);
+	}
+}
