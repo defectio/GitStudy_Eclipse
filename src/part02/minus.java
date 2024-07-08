71e8608a7f331e44b1d13c1c2545c632e88aeacd
defectio <defectio916@gmail.com>
2024-07-08 10:48:47 +0900

    minus 기능 추가

---
+++ src/part02/minus.java
@@ -0,0 +1,12 @@
+package part02;
+
+public class minus {
+
+	public static int minus(int a, int b) {
+		return a - b;
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(minus(5, 3));
+	}
+}
