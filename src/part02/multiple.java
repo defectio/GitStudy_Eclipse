8f4b393d889fdbf7706a4dd08807453157ae8a91
defectio <defectio916@gmail.com>
2024-07-10 08:29:56 +0900

    multiple 기능 추가

---
+++ src/part02/multiple.java
@@ -0,0 +1,13 @@
+package part02;
+
+public class multiple {
+
+	public static int calc(int a, int b) {
+		return a * b;
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(calc(5, 3));
+	}
+
+}
