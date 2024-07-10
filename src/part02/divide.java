c181bd33ef85d438c6ea134443690ef62469ad7f
defectio <defectio916@gmail.com>
2024-07-10 16:10:08 +0900

    divide 기능 추가

---
+++ src/part02/divide.java
@@ -0,0 +1,13 @@
+package part02;
+
+public class divide {
+
+	public static int calc(int a, int b) {
+		return a / b;
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(calc(6, 3));
+	}
+
+}
