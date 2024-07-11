c41d0f8f21b14822da7f94e5770c6de890047fe8
defectio <defectio916@gmail.com>
2024-07-11 09:27:46 +0900

    기능 수정

--- src/part02/multiple.java
+++ src/part02/multiple.java
@@ -7,7 +7,7 @@ public class multiple {
 	}
 	
 	public static void main(String[] args) {
-		System.out.println(calc(5, 3));
+		System.out.println(calc(6, 3));
 		System.out.println(calc(9, 3));
 	}
 
