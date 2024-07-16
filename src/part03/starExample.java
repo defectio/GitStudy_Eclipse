cf6c9e8aff25cc12de88f8757d3a0566a4ef8b86
defectio <defectio916@gmail.com>
2024-07-16 09:59:32 +0900

    1~100까지의 합, 별찍기 추가

--- src/part03/starExample.java
+++ src/part03/starExample.java
@@ -4,7 +4,7 @@ public class starExample {
 
 	public static void main(String[] args) {
 		
-		for (int i = 0 ; i < 5; i++) {
+		for (int i = 0 ; i < 6; i++) {
 			for (int j = 0 ; j < i + 1; j++) {
 				System.out.print("*");
 			}
