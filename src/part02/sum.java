24ce6f80b9b43257782be08f3707164b873c0ab8
defectio <defectio916@gmail.com>
2024-07-17 16:23:55 +0900

    30까지 합으로 변경

--- src/part02/sum.java
+++ src/part02/sum.java
@@ -5,7 +5,7 @@ public class sum {
 	public static void main(String[] args) {
 		
 		int sum = 0;
-		for(int i = 1; i < 51; i++) {
+		for(int i = 1; i < 31; i++) {
 			sum += i;
 		}
 		System.out.println("sum : "+sum);
