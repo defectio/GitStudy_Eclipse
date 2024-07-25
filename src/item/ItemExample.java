fbcde094c5a183b51d1db1b71f389d85154e88ca
defectio <defectio916@gmail.com>
2024-07-25 14:24:30 +0900

    상품 수정 기능 추가

--- src/item/ItemExample.java
+++ src/item/ItemExample.java
@@ -5,6 +5,7 @@ public class ItemExample {
 	public static void main(String[] args) {
 		System.out.println("1. 상품 등록 기능 추가");
 		System.out.println("2. 상품 목록 기능 추가");
+		System.out.println("3. 상품 수정 기능");
 	}
 
 }
