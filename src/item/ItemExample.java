be5dff1ac9930a1ab63ea31b1f81d07cc5c594ac
defectio <defectio916@gmail.com>
2025-05-22 23:44:05 +0900

    검색 기능 추가

--- src/item/ItemExample.java
+++ src/item/ItemExample.java
@@ -8,6 +8,8 @@ public class ItemExample {
 		System.out.println("3. 상품 목록 기능에 필터링 추가");
 		System.out.println("4. 정렬기능 추가");
 		System.out.println("5. 상품 등록 시 임시저장 기능 추가");
+		System.out.println("6. [목록] 목록 페이지에서 검색기능 추가");
+		
 	}
 
 }
