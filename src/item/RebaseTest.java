2732b4fe7060a64e9454ab08a625cca577f9b7ef
defectio <defectio916@gmail.com>
2025-05-24 22:27:05 +0900

    [rebase] rebase-1

--- src/item/RebaseTest.java
+++ src/item/RebaseTest.java
@@ -5,6 +5,8 @@ public class RebaseTest {
 	public static void main(String[] args) {
 		System.out.println("[rebase] develop1");
 		System.out.println("[rebase] develop2");
+		
+		System.out.println("[rebase] rebase-1");
 	}
 
 }
