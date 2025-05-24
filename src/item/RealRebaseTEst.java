f9c20613298a43d8fbde2390656cb9b62652c972
defectio <defectio916@gmail.com>
2025-05-24 22:49:38 +0900

    다시 interactive rebase

--- src/item/RealRebaseTEst.java
+++ src/item/RealRebaseTEst.java
@@ -2,4 +2,19 @@ package item;
 
 public class RealRebaseTEst {
 
+	public static void main(String[] args) {
+		System.out.println("[real] develop-1");
+		
+		System.out.println("[real] rebase-1");
+		System.out.println("[real] rebase-2");
+		System.out.println("[real] rebase-3");
+		
+		System.out.println("[real] interactive rebase1");
+		System.out.println("[real] interactive rebase2");
+		System.out.println("[real] interactive rebase3");
+		
+		System.out.println("다시 interactive rebase 1");
+		System.out.println("다시 interactive rebase 2");
+		
+	}
 }
