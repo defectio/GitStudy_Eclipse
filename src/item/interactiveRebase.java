fe29017d0b013344853c6980c96707b99e62eee7
defectio <defectio916@gmail.com>
2025-05-26 23:33:58 +0900

    abc

---
+++ src/item/interactiveRebase.java
@@ -0,0 +1,11 @@
+package item;
+
+public class interactiveRebase {
+
+	public static void main(String[] args) {
+		System.out.println("a");
+		System.out.println("b");
+		System.out.println("c");
+	}
+
+}
