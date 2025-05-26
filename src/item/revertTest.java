11fd4d08bfbdc1c62e299ccaef2f971741359e7d
defectio <defectio916@gmail.com>
2025-05-26 23:32:35 +0900

    revert b

--- src/item/revertTest.java
+++ src/item/revertTest.java
@@ -5,9 +5,6 @@ public class revertTest {
 	public static void main(String[] args) {
 		System.out.println("a");
 		System.out.println("b");
-		System.out.println("c");
-		System.out.println("d");
-		System.out.println("e");
 	}
 
 }
